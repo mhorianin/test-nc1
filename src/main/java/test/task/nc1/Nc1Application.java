06101bd52129b9e0bd4603ee8f34eb8052e14f7d
Mykola Horianin <anyavic2379@gmail.com>
2024-08-02 15:13:26 +0300

    first commit

---
+++ src/main/java/test/task/nc1/Nc1Application.java
@@ -0,0 +1,13 @@
+package test.task.nc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/test/task/nc1/Nc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package test.task.nc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
